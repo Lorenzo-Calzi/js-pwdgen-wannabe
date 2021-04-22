c1f2ecfd7d9a6addd8913e8befe9480fc6eec2d7
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-04-22 19:40:38 +0200

    Aggiunti i dati inseriti dall'utente e foglio css

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -1,15 +1,15 @@
 //Chiedere all'utente il Nome
 var userName = prompt('Come ti chiami ?')
-
+document.getElementById('name').innerHTML = 'Nome : ' + userName;
 
 //Chiedere all'utente il Cognome
 var lastName = prompt("Qual'è il tuo cognome ?")
-
+document.getElementById('lastname').innerHTML = 'Cognome : ' + lastName;
 
 //Chiedere all'utente il Colore Preferito 
 var favColor = prompt("Qual'è il tuo colore preferito ?")
-
+document.getElementById('color').innerHTML = 'Colore : ' + favColor;
 
 //Unire i vari risultati per creare un password
 document.getElementById('greetings').innerHTML = 'Ciao '+ userName;
-document.getElementById('password').innerHTML = 'La tua password è : ' + userName + lastName + favColor + '21';
+document.getElementById('password').innerHTML =  userName + lastName + favColor + '21';
