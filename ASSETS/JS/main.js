27583a37278093e3a4d29366e5a63fb260a6033e
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-04-22 14:26:36 +0200

    Aggiunti attributi var

---
+++ ASSETS/JS/main.js
@@ -0,0 +1,13 @@
+//Chiedere all'utente il Nome
+var userName = prompt('Come ti chiami ?')
+
+
+//Chiedere all'utente il Cognome
+var lastName = prompt("Qual'è il tuo cognome ?")
+
+
+//Chiedere all'utente il Colore Preferito 
+var favColor = prompt("Qual'è il tuo colore preferito ?")
+
+
+//Unire i vari risultati per creare un password
